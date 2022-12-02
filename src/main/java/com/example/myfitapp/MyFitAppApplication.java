05bfd998ed7190bd11d668a26b829470a505216a
Justin Hollmer <justin.hollmer@gmail.com>
2022-12-02 09:29:15 -0600

    first commit

---
+++ src/main/java/com/example/myfitapp/MyFitAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfitapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFitAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFitAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myfitapp/MyFitAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfitapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFitAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
