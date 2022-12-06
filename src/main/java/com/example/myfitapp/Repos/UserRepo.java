1551a2f531129e6e3db23b1dd3e3b0cd6248825b
Justin Hollmer <justin.hollmer@gmail.com>
2022-12-06 14:23:37 -0600

    added password hashing and login functionality

--- src/main/java/com/example/myfitapp/Repos/UserRepo.java
+++ src/main/java/com/example/myfitapp/Repos/UserRepo.java
@@ -4,4 +4,6 @@ import com.example.myfitapp.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // This repo gives all the methods that will be needed to manipulate the database data.
-public interface UserRepo extends JpaRepository<User, Long> {}
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
