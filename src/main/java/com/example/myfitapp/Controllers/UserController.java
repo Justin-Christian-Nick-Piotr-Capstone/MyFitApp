f87e54ddd78db9c85876fa0f8937a67b60417021
Justin Hollmer <justin.hollmer@gmail.com>
2022-12-07 11:20:44 -0600

    Pushing to avoid merge conflicts

--- src/main/java/com/example/myfitapp/Controllers/UserController.java
+++ src/main/java/com/example/myfitapp/Controllers/UserController.java
@@ -3,9 +3,8 @@ package com.example.myfitapp.Controllers;
 
 import com.example.myfitapp.Models.User;
 import com.example.myfitapp.Repos.UserRepo;
-import jakarta.annotation.security.PermitAll;
-import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
