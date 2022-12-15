5619bf2535bd3101d4a02a961dd9026193ebc154
Justin Hollmer <justin.hollmer@gmail.com>
2022-12-15 12:00:20 -0600

    currently adding in the functionality for the daily logs

--- src/main/java/com/example/myfitapp/Repos/DailyLogRepo.java
+++ src/main/java/com/example/myfitapp/Repos/DailyLogRepo.java
@@ -1,7 +1,11 @@
 package com.example.myfitapp.Repos;
 
 import com.example.myfitapp.Models.DailyLog;
+import com.example.myfitapp.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DailyLogRepo extends JpaRepository<DailyLog, Long> {
+    List<DailyLog> findDailyLogByUser(User user);
 }
